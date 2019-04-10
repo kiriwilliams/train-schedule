55cdc059813c81713b7f1f1209f709039783bb91
sharky <zeebing@gmail.com>
2019-04-10 16:00:41 -0400

    got database push working

--- assets/app.js
+++ assets/app.js
@@ -10,31 +10,30 @@ $(document).ready(function(){
       var elapsedTime = time.diff(this.firstTrain,"minutes");
       var nextTrainIn = elapsedTime % this.frequency;
       return nextTrainIn;
-    },
-    this.addToSchedule(){
-      var row = $("<tr>");
-      var 
     }
-  }
+    // this.addToSchedule() = function(){
+    //   var row = $("<tr>");
+  // }
+}
 
   var trains = [];
-  alert("hi");
-  Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDEghsoO5vEvNPD9FwXSDONLeHtLz3FdPQ",
-  authDomain: "newproject-e5470.firebaseapp.com",
-  databaseURL: "https://newproject-e5470.firebaseio.com",
-  projectId: "newproject-e5470",
-  storageBucket: "newproject-e5470.appspot.com",
-  messagingSenderId: "941985779737"
-};
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDEghsoO5vEvNPD9FwXSDONLeHtLz3FdPQ",
+    authDomain: "newproject-e5470.firebaseapp.com",
+    databaseURL: "https://newproject-e5470.firebaseio.com",
+    projectId: "newproject-e5470",
+    storageBucket: "newproject-e5470.appspot.com",
+    messagingSenderId: "941985779737"
+  };
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
 database.ref().on("child_added", function(snapshot){
   //grab data from the database when new child added
-  var name = shapshot.val().name;
+  var name = snapshot.val().name;
   var destination = snapshot.val().destination;
   var firstTrain = snapshot.val().firstTrain;
   var frequency = snapshot.val().frequency;
@@ -44,12 +43,12 @@ database.ref().on("child_added", function(snapshot){
   trains.push(train);
 })
 
-$(".modal-footer").on("click", function (e) {
+$("#submit").on("click", function (e) {
   //grab user input
   var name = $("#train-name").val().trim();
   var destination = $("#train-destination").val().trim();
-  var firstTrain = $("#train-frst").val().trim();
-  var frequency = $("train-frequency").val().trim();
+  var firstTrain = $("#train-first").val().trim();
+  var frequency = $("#train-frequency").val().trim();
 
   //push to database
   database.ref().push({
